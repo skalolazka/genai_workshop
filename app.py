6ff5297595528c8c25d92c84c2f5ea6d5ccec73e
Natalia Strelkova <nstrelkova@google.com>
2023-11-27 08:25:17 +0000

    initial

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from vertexai.language_models import ChatModel, InputOutputTextPair
+
+app = Flask(__name__)
+
+@app.route('/')
+def science_tutoring(temperature: float = 0.2) -> None:
+    chat_model = ChatModel.from_pretrained("chat-bison@001")
+
+    # TODO developer - override these parameters as needed:
+    parameters = {
+        "temperature": temperature,  # Temperature controls the degree of randomness in token selection.
+        "max_output_tokens": 256,  # Token limit determines the maximum amount of text output.
+        "top_p": 0.95,  # Tokens are selected from most probable to least until the sum of their probabilities equals the top_p value.
+        "top_k": 40,  # A top_k of 1 means the selected token is the most probable among all tokens.
+    }
+
+    chat = chat_model.start_chat(
+        context="My name is Miles. You are an astronomer, knowledgeable about the solar system.",
+        examples=[
+            InputOutputTextPair(
+                input_text="How many moons does Mars have?",
+                output_text="The planet Mars has two moons, Phobos and Deimos.",
+            ),
+        ],
+    )
+
+    response = chat.send_message(
+        "How many planets are there in the solar system?", **parameters
+    )
+    print(f"Response from Model: {response.text}")
+
+    return response
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8000)
